942fa43e6d0e2ecc934b9f8584ff2b1e4d960278
Daniel Carlebach <danieldcarlebach@gmail.com>
2024-07-09 14:14:20 -0400

    Create project skeleton

---
+++ src/SINTRA_CHEOPS.jl
@@ -0,0 +1,8 @@
+module SINTRA_CHEOPS
+
+include("downloading.jl")
+include("image_processing.jl")
+include("ephemeris_calculation.jl")
+include("summary_generation.jl")
+
+end # module SINTRA_CHEOPS
